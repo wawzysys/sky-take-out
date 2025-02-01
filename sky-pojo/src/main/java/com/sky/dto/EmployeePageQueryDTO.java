08747365ea9a8cd70f5edfc37bd9d6aa97d108a7
wawzysys <1916040083@qq.com>
2025-02-01 13:32:41 +0800

    day 2 员工分页查询代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,19 +1,24 @@
 package com.sky.dto;
 
 import lombok.Data;
-
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class EmployeePageQueryDTO implements Serializable {
 
-    //员工姓名
+    //员工姓名，可选
     private String name;
 
-    //页码
-    private int page;
+    //页码，必填，最小值为1
+    @NotNull(message = "页码不能为空")
+    @Min(value = 1, message = "页码最小为1")
+    private Integer page;
 
-    //每页显示记录数
-    private int pageSize;
+    //每页显示记录数，必填，最小值为1
+    @NotNull(message = "每页显示记录数不能为空")
+    @Min(value = 1, message = "每页显示记录数最小为1")
+    private Integer pageSize;
 
 }
